c62eb6bcad54d99820deade27afcb3c3093be48b
jiksi <zhicxiap99@gmail.com>
2026-04-15 19:11:51 +0800

    Add input fields for name and message in Ucapan component with default value from search params

--- components/ucapan.tsx
+++ components/ucapan.tsx
@@ -1,7 +1,10 @@
 import Navigation from "./navigation";
 import Flower from "./flower";
+import { useSearchParams } from "next/navigation";
 
 export default function Ucapan() {
+  const to = useSearchParams().get("to");
+
   return (
     <section
       id="ucapan"
@@ -14,15 +17,18 @@ export default function Ucapan() {
         </h2>
         <div className="flex flex-col items-center gap-2">
           <input
+            name="nama"
+            id="nama"
             type="text"
             placeholder="Nama"
-            className="font-heading placeholder:font-heading text-primary placeholder:text-primary/20 bg-secondary focus:ring-secondary w-xs rounded-md border-2 px-4 py-2 text-xs placeholder:text-xs focus:ring-2 focus:ring-offset-2 focus:outline-none"
+            defaultValue={to || ""}
+            className="font-heading placeholder:font-heading text-primary placeholder:text-primary/20 bg-secondary focus:ring-secondary w-xs rounded-md border-2 p-2 text-xs placeholder:text-xs focus:ring-2 focus:ring-offset-2 focus:outline-none"
           />
           <textarea
             name="ucapan"
             id="ucapan"
             placeholder="Ucapan..."
-            className="font-heading placeholder:font-heading text-primary placeholder:text-primary/20 bg-secondary focus:ring-secondary h-20 w-xs rounded-md border-2 px-4 py-2 text-sm placeholder:text-xs focus:ring-2 focus:ring-offset-2 focus:outline-none"
+            className="font-heading placeholder:font-heading text-primary placeholder:text-primary/20 bg-secondary focus:ring-secondary h-20 w-xs rounded-md border-2 p-2 text-xs placeholder:text-xs focus:ring-2 focus:ring-offset-2 focus:outline-none"
           ></textarea>
         </div>
         <button className="bg-secondary text-primary hover:bg-secondary/80 focus:ring-secondary font-heading rounded-md border-2 px-4 py-2 text-sm focus:ring-2 focus:ring-offset-2 focus:outline-none">
